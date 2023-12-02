375147a57b5a701782e5671c3cd1ed9b5d9895a4
Cameron Spickert <cspickert@users.noreply.github.com>
2023-12-02 16:22:34 +0000

    Day 2 part 1 and 2

---
+++ day02.py
@@ -0,0 +1,45 @@
+import base
+import operator
+from functools import reduce
+
+
+class Solution(base.Solution):
+    def load_data(self, input):
+        def parse_round(text):
+            return {b: int(a) for a, b in [item.split() for item in text.split(", ")]}
+
+        def parse_game(text):
+            chunks = text.split(": ")
+            game_id = int(chunks[0].split()[1])
+            rounds = [parse_round(item) for item in chunks[1].split("; ")]
+            return (game_id, rounds)
+
+        return [parse_game(line) for line in input.splitlines()]
+
+    def part1(self, data):
+        return sum(
+            game_id
+            for game_id, rounds in data
+            if all(
+                amount <= {"red": 12, "green": 13, "blue": 14}[color]
+                for game_round in rounds
+                for color, amount in game_round.items()
+            )
+        )
+
+    def part2(self, data):
+        return sum(
+            reduce(
+                operator.mul,
+                (
+                    max(
+                        amount
+                        for game_round in game
+                        for round_color, amount in game_round.items()
+                        if round_color == color
+                    )
+                    for color in ["red", "green", "blue"]
+                ),
+            )
+            for game_id, game in data
+        )
---
+++ tests/test_day02.py
@@ -0,0 +1,5 @@
+def test_part1(run_part):
+    assert run_part() == 8
+
+def test_part2(run_part):
+    assert run_part() == 2286

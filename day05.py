7051718bfd3b85fdf45b65e5d6ffcf919cced567
Cameron Spickert <cameron@checkhq.com>
2023-12-06 10:55:52 -0500

    Day 5 part 2 (wip)

--- day05.py
+++ day05.py
@@ -7,17 +7,65 @@ def parse_ints(text):
     return [int(value) for value in text.split()]
 
 
+def range_and(a, b):
+    # a & b
+    max_min = max(a.start, b.start)
+    min_max = min(a.stop, b.stop)
+    return range(max_min, min_max)
+
+
+def range_sub(a, b):
+    # a - b
+    overlap = range_and(a, b)
+    if not overlap:
+        return [a]
+    left = range(a.start, overlap.start)
+    right = range(overlap.stop, a.stop)
+    return [x for x in [left, right] if x]
+
+
+def range_coalesce(ranges):
+    ranges = sorted([r for r in ranges if r], key=lambda r: (r.start, r.stop))
+    result = [ranges[0]]
+    for b in ranges[1:]:
+        a = result.pop()
+        if range_and(a, b) or a.stop == b.start:
+            x = range(a.start, b.stop)
+            result.append(x)
+        else:
+            result.extend((a, b))
+    return result
+
+
 class Map:
     def __init__(self, name, values):
         self.name = name
         self.values = values
 
-    def __getitem__(self, source):
+    def map_value(self, input_value):
         for dest_start, src_start, range_len in self.values:
             src_range = range(src_start, src_start + range_len)
-            if source in src_range:
-                return dest_start + source - src_start
-        return source
+            if input_value in src_range:
+                return dest_start + input_value - src_start
+        return input_value
+
+    def map_range(self, input_range):
+        output_ranges = []
+        for dest_start, src_start, range_len in self.values:
+            src_range = range(src_start, src_start + range_len)
+            output_ranges.extend(
+                # Unmapped input values
+                range_sub(input_range, src_range),
+            )
+
+            src_range_overlap = range_and(src_range, input_range)
+            if src_range_overlap:
+                mapped_start = src_range_overlap.start - src_start + dest_start
+                mapped_stop = src_range_overlap.stop - src_start + dest_start
+                output_ranges.append(range(mapped_start, mapped_stop))
+
+        result = range_coalesce(output_ranges)
+        return result
 
 
 class Solution(base.Solution):
@@ -35,8 +83,26 @@ class Solution(base.Solution):
         return (seeds, maps)
 
     def get_location(self, maps, seed):
-        return reduce(lambda result, map: map[result], maps, seed)
+        return reduce(lambda result, next_map: next_map.map_value(result), maps, seed)
 
     def part1(self, data):
         seeds, maps = data
         return min(self.get_location(maps, seed) for seed in seeds)
+
+    def part2(self, data):
+        seed_data, maps = data
+        all_ranges = [
+            range(range_start, range_start + range_len)
+            for range_start, range_len in [
+                seed_data[i : i + 2] for i in range(0, len(seed_data), 2)
+            ]
+        ]
+
+        for next_map in maps:
+            mapped_ranges = []
+            for next_range in all_ranges:
+                mapped_ranges.extend(next_map.map_range(next_range))
+            all_ranges = range_coalesce(mapped_ranges)
+
+        result = min(all_ranges, key=lambda r: r.start).start
+        return result
--- tests/test_day05.py
+++ tests/test_day05.py
@@ -3,4 +3,4 @@ def test_part1(run_part):
 
 
 def test_part2(run_part):
-    assert run_part() == 0
+    assert run_part() == 46

54853d9fedced4a3644e79118ed04791b58b884b
Cameron Spickert <cameron@checkhq.com>
2023-12-07 09:15:54 -0500

    Day 7 part 2 wip

--- day07.py
+++ day07.py
@@ -4,24 +4,44 @@ import base
 
 
 class Solution(base.Solution):
-    def __init__(self):
-        self.card_mapping = {str(i): i for i in range(1, 10)} | {
-            c: i for i, c in enumerate("TJQKA", start=10)
-        }
-
-    def load_data(self, input):
-        return [
-            (self.parse_hand(a), int(b))
-            for a, b in [line.split() for line in input.splitlines()]
-        ]
-
     def part1(self, data):
-        return sum(i * bid for i, (_, bid) in enumerate(sorted(data), start=1))
+        hands = self.load_hands(data)
+        return self.total_winnings(hands)
+
+    def part2(self, data):
+        hands = self.load_hands(data, jokers=True)
+        return self.total_winnings(hands)
 
     # Helpers
 
-    def parse_hand(self, hand):
+    def total_winnings(self, hands):
+        return sum(i * bid for i, (_, bid) in enumerate(sorted(hands), start=1))
+
+    def load_hands(self, input, jokers=False):
+        return [
+            (self.parse_hand(a, jokers), int(b))
+            for a, b in [line.split() for line in input.splitlines()]
+        ]
+
+    def parse_hand(self, hand, jokers):
+        card_mapping = self.card_mapping(jokers)
+        counts = Counter(hand)
+        if jokers:
+            try:
+                (best_card, _), *_ = Counter(hand.replace("J", "")).most_common()
+            except ValueError:
+                best_card = "A"
+            best_hand = hand.replace("J", best_card)
+            counts = Counter(best_hand)
         return (
-            tuple(sorted(Counter(hand).values(), reverse=True)),
-            tuple(self.card_mapping[k] for k in hand),
+            tuple(value for _, value in counts.most_common()),
+            tuple(card_mapping[k] for k in hand),
         )
+
+    def card_mapping(self, jokers):
+        card_mapping = {str(i): i for i in range(2, 10)} | {
+            c: i for i, c in enumerate("TJQKA", start=10)
+        }
+        if jokers:
+            card_mapping["J"] = 1
+        return card_mapping
--- tests/test_day07.py
+++ tests/test_day07.py
@@ -1,2 +1,6 @@
 def test_part1(run_part):
     assert run_part() == 6440
+
+
+def test_part2(run_part):
+    assert run_part() == 5905

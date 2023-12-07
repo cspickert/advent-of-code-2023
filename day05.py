f63207d10f15889d36eef34ed5752588a7c272a3
Cameron Spickert <cameron@checkhq.com>
2023-12-06 21:14:48 -0500

    Day 5 part 2

--- day05.py
+++ day05.py
@@ -50,19 +50,30 @@ class Map:
         return input_value
 
     def map_range(self, input_range):
+        input_ranges = [input_range]
         output_ranges = []
-        for dest_start, src_start, range_len in self.values:
-            src_range = range(src_start, src_start + range_len)
-            output_ranges.extend(
-                # Unmapped input values
-                range_sub(input_range, src_range),
-            )
 
-            src_range_overlap = range_and(src_range, input_range)
-            if src_range_overlap:
-                mapped_start = src_range_overlap.start - src_start + dest_start
-                mapped_stop = src_range_overlap.stop - src_start + dest_start
-                output_ranges.append(range(mapped_start, mapped_stop))
+        for dest_start, src_start, range_len in self.values:
+            unmapped_input_ranges = []
+
+            for input_range in input_ranges:
+                src_range = range(src_start, src_start + range_len)
+
+                src_range_overlap = range_and(src_range, input_range)
+                if src_range_overlap:
+                    mapped_start = src_range_overlap.start - src_start + dest_start
+                    mapped_stop = src_range_overlap.stop - src_start + dest_start
+                    output_ranges.append(range(mapped_start, mapped_stop))
+
+                    sub_result = range_sub(input_range, src_range_overlap)
+                    unmapped_input_ranges.extend(sub_result)
+
+                else:
+                    unmapped_input_ranges.append(input_range)
+
+            input_ranges = unmapped_input_ranges
+
+        output_ranges.extend(input_ranges)
 
         result = range_coalesce(output_ranges)
         return result
